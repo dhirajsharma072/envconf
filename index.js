09448bf85eafafde5f31d5ca4348dd919a35d5fa
Dhiraj Sharma <dhirajsharma072@gmail.com>
2017-06-22 11:55:44 +0800

    converted into es6 syntax to make compatible with es5

--- index.js
+++ index.js
@@ -1,23 +1,25 @@
-const Promise = require('bluebird');
-const readFile = Promise.promisify(require('fs').readFile);
-const env = require('getenv');
+var Promise = require('bluebird');
+var readFile = Promise.promisify(require('fs').readFile);
+var env = require('getenv');
 require('dotenv').config();
 
 
-const checkEnvironmentVariables = ()=> {
-  return readFile(`${__dirname}/.env.tpl`,'utf8')
-    .then(file => {
-      const envList = file && file.split(/(?:\n|\r\n|\r)/)
+const checkEnvironmentVariables = function () {
+  return readFile(__dirname + '/.env.tpl', 'utf8')
+    .then(function (file) {
+      var envList = file && file.split(/(?:\n|\r\n|\r)/)
           .filter((p) => !(p.match(/^(\/\/ |\/\*|#)/)) && (p.indexOf('=') >= 0))
           .map(line=>line.split('=')[0].trim())
           .filter(envVar => !process.env[envVar]);
       if (envList && envList.length > 0) {
-        throw new Error(`Please set environment variable ${envList.toString()}`);
+        throw new Error('Please set environment variable ' + envList.toString());
       }
       return envList;
     });
 };
-const getenv = ()=> env;
+const getenv = function () {
+  return env;
+};
 
 module.exports = {
   checkEnvironmentVariables: checkEnvironmentVariables,
